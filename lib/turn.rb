67f31f44af61ff78041f488f66b3ca7ad32062a2
Barbara Johnson <johnson.barbara@gmail.com>
2019-03-31 05:04:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,6 +3,9 @@ def turn
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true 
+    move(board, index, "X")
+    display_board(board)
+  end
     
   
 end
